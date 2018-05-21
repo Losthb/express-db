2e1864e96e29a1e6d88147dbd56311bd007a1cc1
Losthb <398516771@qq.com>
2018-05-21 18:42:42 +0800

    Initial commit

---
+++ conf/db.js
@@ -0,0 +1,11 @@
+// conf/db.js
+// MySQL数据库联接配置
+module.exports = {
+    mysql: {
+        host: '192.168.174.168',
+        user: 'root',
+        password: 'lovezdy',
+        database:'lovezdy', // 前面建的user表位于这个数据库中
+        port: 3306
+    }
+};
